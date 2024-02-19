0eb766777ca5714caf23ff4f420cf25cd7992e33
RaniRam8149 <156404927+RaniRam8149@users.noreply.github.com>
2024-02-19 14:51:25 +0530

    Create FourQuestion.py

---
+++ FourQuestion.py
@@ -0,0 +1,13 @@
+Q4. How can we achieve data abstraction?
+
+Data abstraction in programming refers to the concept of hiding complex implementation details and showing only the essential features of an object. In object-oriented programming, achieving data abstraction involves two main techniques:
+
+1.Encapsulation:
+Encapsulation involves bundling the data (attributes or properties) and methods (functions or behaviors) that operate on the data into a single unit, usually a class.
+The class exposes only the necessary functionalities to interact with its data, hiding the internal implementation details.
+Access to the data is controlled through well-defined interfaces, using methods or properties.
+
+2.Abstract Classes or Interfaces:
+Abstract classes or interfaces define a blueprint that specifies a set of methods or properties without providing the implementation.
+Subclasses inherit from these abstract classes or implement these interfaces, providing concrete implementations for the abstract methods or properties.
+This ensures that all subclasses adhere to a common structure, promoting consistency and abstraction.
