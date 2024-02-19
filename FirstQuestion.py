fbee1800434f3e77203215c2a9814ce1fc0415f5
RaniRam8149 <156404927+RaniRam8149@users.noreply.github.com>
2024-02-19 14:45:32 +0530

    Create FirstQuestion.py

---
+++ FirstQuestion.py
@@ -0,0 +1,13 @@
+Q1. What is Abstraction in Oops? Explain with an example
+Abstraction in Object-Oriented Programming (OOP) is the concept of simplifying complex systems by focusing on essential properties and hiding unnecessary details.
+class Vehicle:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
